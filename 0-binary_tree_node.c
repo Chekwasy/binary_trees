6df2492eb3bb6ef5c8281c4a07d9a6e82ce8d99c
Richard <richardchekwas@gmail.com>
2023-08-01 12:50:28 +0100

    aa

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,15 +9,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *root;
+	binary_tree_t *root;
 
-  root = malloc(sizeof(binary_tree_t));
-  if (root == NULL)
-    return NULL;
+	root = malloc(sizeof(binary_tree_t));
+	if (root == NULL)
+		return NULL;
 
-  root->n = value;
-  root->parent = parent;
-  root->left = NULL;
-  root->right = NULL;
-  return root;
+	root->n = value;
+	root->parent = parent;
+	root->left = NULL;
+	root->right = NULL;
+	return root;
 }
