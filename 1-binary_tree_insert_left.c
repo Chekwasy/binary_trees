01656b42baf1d21d0ed224c6c9a671d797a34a9f
Richard <richardchekwas@gmail.com>
2023-08-02 09:16:16 +0100

    aaa

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	root = malloc(sizeof(binary_tree_t));
+	root = binary_tree_node(parent, value);
 	if (root == NULL)
 		return (NULL);
 
