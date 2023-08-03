85f9b0ff6d84fd8e05e6a7ad656cf84d315f31f3
Richard <richardchekwas@gmail.com>
2023-08-03 23:29:30 +0100

    not easy

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t a = 0, b = 0;
 
-		if(tree->left)
+		if (tree->left)
 			a = 1 + binary_tree_height(tree->left);
 		else
 			a = 0;
